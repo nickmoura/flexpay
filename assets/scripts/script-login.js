04266154dca5ce49947d3aca7a763fe48553ace7
nickmoura <nickmoura26@gmail.com>
2024-02-07 11:23:10 -0300

    improvements

--- assets/scripts/script-login.js
+++ assets/scripts/script-login.js
@@ -1,17 +1,38 @@
-document.addEventListener('DOMContentLoaded', function() {
-	const loginForm = document.getElementById('flexpay-login-form');
+  const inputCnpj = document.getElementById('cnpj')
+  if (inputCnpj) {
+	  inputCnpj.addEventListener('keypress', () => {
+		  let inputlength = inputCnpj.value.length
+		  const keyCode = event.which
   
-	loginForm.addEventListener('submit', function(event) {
-	  event.preventDefault();
-	  const username = document.getElementById('username').value;
-	  const password = document.getElementById('password').value;
+		  if (keyCode < 48 || keyCode > 57) {
+			  event.preventDefault()
+		  }
   
-	  // Simulação de autenticação (substitua por lógica real)
-	  if (username === 'seuUsuario' && password === 'suaSenha') {
-		alert('Login bem-sucedido!');
-		// Redirecionar ou executar ações necessárias após o login
-	  } else {
-		alert('Credenciais inválidas. Tente novamente.');
+		  if (inputlength === 2 || inputlength === 6) {
+			  inputCnpj.value += '.'
+		  } else if (inputlength === 10) {
+			  inputCnpj.value += '/'
+		  } else if(inputlength === 15) {
+			  inputCnpj.value += '-'
+		  }
+	  });
+  }
+  
+  
+  const inputCel = document.getElementById('cel')
+  if (inputCel) {
+  
+	  inputCel.addEventListener('input', () => {
+		  const inputValue = inputCel.value.replace(/\D/g, ''); // Remove caracteres não-numéricos
+		  const formattedValue = formatPhoneNumber(inputValue);
+		  inputCel.value = formattedValue;
+	  });
+  
+	  function formatPhoneNumber(value) {
+		  const match = value.match(/^(\d{2})(\d{5})(\d{0,4})$/);
+		  if (match) {
+			  return `(${match[1]}) ${match[2]}${match[3] ? `-${match[3]}` : ''}`;
+		  }
+		  return value;
 	  }
-	});
-  });
+  }
